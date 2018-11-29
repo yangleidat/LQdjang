89ab55008e8fce931927611310bb1abc72c05719
yangleidat <hdcqyl@126.com>
2018-11-29 23:24:49 +0800

    丰富了用户注册信息

--- mysite/account/admin.py
+++ mysite/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'birth', 'phone')
+    list_filter = ('phone',) #定义右边栏显示的过滤器的内容
+
+admin.site.register(UserProfile, UserProfileAdmin)
