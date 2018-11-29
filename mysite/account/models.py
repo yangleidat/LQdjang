f6846b4217e04041ab33410a2ff56d87eee3c74b
yangleidat <hdcqyl@126.com>
2018-11-29 22:28:47 +0800

    创建userprofile数据库模型

--- mysite/account/models.py
+++ mysite/account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    #使用onetoonefield的含义是通过user这个字段声明UserProfile类与User类之间的关系一对一的
+    user = models.OneToOneField(User, unique=True)
+    birth = models.DateField(blank=True, null=True)
+    phone = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return 'user {}'.format(self.user.username)
